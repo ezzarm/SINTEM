1f63572eb23433ed786afb6ed71c9ffd011305e4
Ezzar Alhafidh <ezzr.lco00@gmail.com>
2026-04-17 13:16:29 +0700

    update: add remember token

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ class User extends Authenticatable
     use HasFactory;
 
     protected $table = 'users';
-
+    protected $rememberTokenName = null;
     public $timestamps = true;
 
     protected $fillable = [
